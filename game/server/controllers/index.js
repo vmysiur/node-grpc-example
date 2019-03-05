5d1d6c2cf34aed8231859ab24bfb15115c45f981
Vladislav Mysiur <vladislav.m@soft2bet.com>
2019-03-05 12:27:26 +0200

    Keep calm and commit

--- game/server/controllers/index.js
+++ game/server/controllers/index.js
@@ -1,13 +1,13 @@
 function getGame(call, callback) {
-    callback(null, {
-        id: call.request.id,
-        title: "Test game title",
-        tags: ['slot', 'poker', 'casino'],
-        keywords: [{tag: 'Key1'}, {tag: 'Key2'}]
-    })
+  callback(null, {
+    id: call.request.id,
+    title: 'Test game title',
+    tags: ['slot', 'poker', 'casino'],
+    keywords: [{ tag: 'Key1' }, { tag: 'Key2' }]
+  })
 
 }
 
 module.exports = {
-    getGame
-};
+  getGame
+}
